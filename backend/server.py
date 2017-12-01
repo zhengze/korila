cf7206058e77e102f5a572001ea96ee09fe77d97
zhengze <zhanghhd@163.com>
2017-12-01 03:13:58 -0800

    ansible command

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 import subprocess
 import logging
 import os
+import ansible
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,15 +11,15 @@ api = Api(app)
 
 class monitor(Resource):
     def get(self):
-        workpath = os.getcwd()
-        return {"workpath": workpath}
+        return {"hello": "world"}
     
     def post(self):
         #child = subprocess.Popen(["fab -f ./scripts/monitor.py test_app_status"], stdout=subprocess.PIPE)
         #res = child.wait()
-        res = os.system("fab -f ./scripts/monitor.py test_app_status")
+        res = os.system("ansible -i ./playbook/hosts devcrp -m shell -a 'ls; pwd'")
+        #res = os.system("fab -f ./scripts/monitor.py test_app_status")
         logging.info("res:", res)
-        return res
+        return {"res": res}
         
 
 
