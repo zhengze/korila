3f254dca389f3c2cdf849e8a0c84423b48731676
600408 <600408@syswin.com>
2018-05-21 23:14:20 -0700

    env.passwords

--- backend/scripts/update_code.py
+++ backend/scripts/update_code.py
@@ -1,5 +1,5 @@
 #import paramiko
-from fabric.api import run, env, roles, execute, local
+from fabric.api import run, env, roles, execute, local, cd, parallel
 import os
 
 #env.hosts = ['172.28.32.49:22', '172.28.32.50:22']
@@ -29,7 +29,13 @@ env.roledefs = {
 }
  
 env.user = 'root'
-env.password = '123.com'
+env.passwords = {
+    'dev_crp': '123.com',
+    'test_lb': '123.com',
+    'test_app': '123.com',
+    'dasha_lb': '123.com',
+    'dasha_app': '123.com',
+} 
 
 supervisorctl_status_cmd = "supervisorctl status"
 keepalived_status_cmd = "systemctl status keepalived"
@@ -48,17 +54,18 @@ def test_lb_status():
     except Exception as ex:
         print "error:", ex
 
-
+@parallel(pool_size=2)
 def update_uop():
     root_path = "/home/zhengze/codes"
-    uop_backend_path = "/".join([root_path, "uop-backend"])
-    crp_backend_path = "/".join([root_path, "uop-crp"])
-    uop_frontend_path = "/".join([root_path, "uop-frontend"])
-    paths = [uop_backend_path, crp_backend_path, uop_frontend_path]
-        
-    for path in paths:
-        print
-        local("cd "+path+";git fetch origin;git reset --hard HEAD;git pull")
+    with cd(root_path):
+        uop_backend_path = "/".join([root_path, "uop-backend"])
+        crp_backend_path = "/".join([root_path, "uop-crp"])
+        uop_frontend_path = "/".join([root_path, "uop-frontend"])
+        paths = [uop_backend_path, crp_backend_path, uop_frontend_path]
+                
+        for path in paths:
+            print
+            local("cd "+path+";git fetch origin;git reset --hard HEAD;git pull")
 
 
 def main():
