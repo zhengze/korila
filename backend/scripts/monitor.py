9ac1cd0383019271a3769461ce8404d540a4ecab
600408 <600408@syswin.com>
2018-05-24 19:30:40 +0800

    env.passwords

--- backend/scripts/monitor.py
+++ backend/scripts/monitor.py
@@ -1,47 +1,49 @@
 #!/usr/bin/env python
-#coding: utf-8
+# coding: utf-8
 
 
-import paramiko
-from fabric.api import run, env, roles, execute, hosts, task
+from fabric.api import run, env, roles, execute, hosts, task, parallel
+from fabric.colors import green
+from settings import UserInfo
 
 
-#IP="172.28.32.50"
-#PORT=22
-#USERNAME="root"
-#PASSWORD="123.com"
-#CMD="supervisorctl status"
-#
-#ssh = paramiko.SSHClient()
-#ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#ssh.connect(IP, PORT, USERNAME, PASSWORD, timeout=5)
-#stdin, stdout, stderr = ssh.exec_command(CMD)
-#print stdout.readlines()
-#ssh.close()
+userinfo = UserInfo()
 
 supervisorctl_status_cmd = "supervisorctl status"
 keepalived_status_cmd = "systemctl status keepalived"
+nginx_status_cmd = "service nginx status"
 
-env.roledefs = {   
-    'dev_crp' : ['172.28.32.32'],  
-    'test_lb' : ['172.28.32.51', '172.28.32.53'], 
-    'test_app' : ['172.28.32.49', '172.28.32.50'],  
+env.roledefs = {
+    'dev_crp': ['172.28.32.32'],
+    'test_lb': ['172.28.32.51', '172.28.32.53'],
+    'test_app': ['172.28.32.49', '172.28.32.50'],
     'dasha_lb': ["172.28.26.199", "172.28.26.200"],
     'dasha_app': ["172.28.26.217", "172.28.26.212"],
-}  
-  
-env.passwords = {
-    'dev_crp': '123.com',
-    'test_lb': '123.com',
-    'test_app': '123.com',
-    'dasha_lb': '123.com',
-    'dasha_app': '123.com',
-} 
+}
 
+env.users = {
+    'dev_crp': userinfo.get_user(),
+    'test_lb': userinfo.get_user(),
+    'test_app': userinfo.get_user(),
+    'dasha_lb': userinfo.get_user(),
+    'dasha_app': userinfo.get_user(),
+}
+env.passwords = {
+    'dev_crp': userinfo.get_password(),
+    'test_lb': userinfo.get_password(),
+    'test_app': userinfo.get_password(),
+    'dasha_lb': userinfo.get_password(),
+    'dasha_app': userinfo.get_password(),
+}
+
+
+@task
 @roles("dev_crp")
 def dev_crp_status():
     run(supervisorctl_status_cmd)
 
+
+@task
 @roles("test_lb")
 def test_lb_status():
     try:
@@ -49,23 +51,29 @@ def test_lb_status():
     except Exception as e:
         print("error:", e)
 
+
+@task
 @roles("test_app")
 def test_app_status():
     run(supervisorctl_status_cmd)
 
+
+@task
 @roles("dasha_lb")
 def dasha_lb_status():
     run(keepalived_status_cmd)
 
+
+@task
 @roles("dasha_app")
 def dasha_app_status():
     run(supervisorctl_status_cmd)
 
 
+@task
 def main():
     execute(dev_crp_status)
     execute(test_lb_status)
     execute(test_app_status)
     execute(dasha_lb_status)
     execute(dasha_app_status)
-
