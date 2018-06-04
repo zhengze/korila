df2e6b25807250c535f6bcd25ef9b8adbdeda025
600408 <600408@syswin.com>
2018-06-04 19:01:19 +0800

    optimize update_uop

--- backend/scripts/local_update.py
+++ backend/scripts/local_update.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python env
 
 
-from fabric.api import run, env, roles, execute, local, cd, parallel, task
+import os
+from fabric.api import run, env, roles, execute, local, cd, lcd, parallel, task
 
 env.executable = '/bin/bash'
 
@@ -10,18 +11,14 @@ env.executable = '/bin/bash'
 @parallel
 def update_uop():
     root_path = "/home/zhengze/codes"
-    with cd(root_path):
-        uop_backend_path = "/".join([root_path, "uop-backend"])
-        crp_backend_path = "/".join([root_path, "uop-crp"])
-        uop_frontend_path = "/".join([root_path, "uop-frontend"])
-        paths = [uop_backend_path, crp_backend_path, uop_frontend_path]
+    paths = ["uop-frontend", "uop-backend", "uop-crp"]
 
-        def update(path):
-            local(
-                "cd " +
-                path +
-                ";git fetch origin;git reset --hard HEAD;git pull")
-        map(update, paths)
+    def update(source_path):
+        path = os.path.join(root_path, source_path)
+        with lcd(path):
+            local("git fetch origin && git reset --hard HEAD && git pull")
+
+    map(update, paths)
 
 
 @task
