a41d7419499aca35b44a3e0bc8bafaeedc28e8fa
600408 <600408@syswin.com>
2018-06-14 16:46:27 +0800

    load env from path

--- backend/scripts/settings.py
+++ backend/scripts/settings.py
@@ -1,13 +1,18 @@
 import os
+from os.path import join, dirname
 from dotenv import load_dotenv, find_dotenv
 
 
 class UserInfo(object):
     def __init__(self):
-        load_dotenv(find_dotenv())
+        # load_dotenv(find_dotenv())
+        dotenv_path = join(dirname(__file__), '../.env')
+        load_dotenv(dotenv_path=dotenv_path)
 
     def get_user(self):
         return os.getenv("USER")
 
     def get_password(self):
         return os.getenv("PASSWORD")
+
+
