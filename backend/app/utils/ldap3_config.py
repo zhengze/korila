6c17ceebc525d00f7f4bfa8d2769e1ed6da19850
600408 <600408@syswin.com>
2018-07-11 14:05:53 +0800

    code format

--- backend/app/utils/ldap3_config.py
+++ backend/app/utils/ldap3_config.py
@@ -9,14 +9,17 @@ password = 'syswin#'
 base_dn = 'dc=syswin,dc=com'
 #userinput = '600289'
 userinput = '146997'
-filter_field = "(&(|(cn=*%(input)s*)(sAMAccountName=*%(input)s*))(sAMAccountName=*))" %{'input': userinput}
-attrs = ['sAMAccountName', 'mail', 'givenName', 'sn', 'department', 'telephoneNumber', 'displayName']
+filter_field = "(&(|(cn=*%(input)s*)(sAMAccountName=*%(input)s*))(sAMAccountName=*))" % {
+    'input': userinput}
+attrs = ['sAMAccountName', 'mail', 'givenName', 'sn',
+         'department', 'telephoneNumber', 'displayName']
 
 
 class LdapConn(object):
     def __init__(self, servername, user, password):
         self.server = Server(servername)
-        self.conn = Connection(servername, user=user, password=password, auto_bind=True)
+        self.conn = Connection(servername, user=user,
+                               password=password, auto_bind=True)
 
     def search_user(self, base_dn, filter_field, attrs):
         result = self.conn.search(base_dn, filter_field, attributes=attrs)
@@ -26,7 +29,8 @@ class LdapConn(object):
         return None
 
     def ldap_auth(self, dn, password):
-        conn = Connection(self.server, user=dn, password=password, check_names=True, lazy=False, raise_exceptions=False)
+        conn = Connection(self.server, user=dn, password=password,
+                          check_names=True, lazy=False, raise_exceptions=False)
         conn.bind()
         return conn.result
 
@@ -39,7 +43,7 @@ if __name__ == "__main__":
         dn = entry["dn"]
         result = ldapobj.ldap_auth(dn, 'Wing!1234')
         if result["description"] == "success":
-            print((True, attr_dict["mail"], attr_dict["sAMAccountName"], attr_dict["givenName"]))
+            print(
+                (True, attr_dict["mail"], attr_dict["sAMAccountName"], attr_dict["givenName"]))
         else:
             print("auth fail")
-
